c144b245db9a48e58b64a411151c08627a23d44f
Nick D <nicolas.k.delbue@gmail.com>
2026-08-08 16:33:24 -0400

    Change
    Did not do the raycasting instead added a PolygonCollider2D which now links up to the vertices created and makes the fov into a trigger for the player

--- HiddenGameJam/Assets/Scripts/Lose/FoVPlayerLose.cs
+++ HiddenGameJam/Assets/Scripts/Lose/FoVPlayerLose.cs
@@ -1,13 +1,18 @@
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Diagnostics;
 
 public class FoVPlayerLose : MonoBehaviour
 {
+    [SerializeField] string PlayerTag;
+    PolygonCollider2D polyCollider;
     private void Start()
     {
         Mesh mesh = new Mesh();
 
         GetComponent<MeshFilter>().mesh = mesh;
+        polyCollider = GetComponent<PolygonCollider2D>();
 
         float fov = 90f;
         Vector3 origin = Vector3.zero;
@@ -20,8 +25,6 @@ public class FoVPlayerLose : MonoBehaviour
         Vector3[] vertices = new Vector3[rayCount+1+1];
         Vector2[] uv = new Vector2[vertices.Length];
         int[] triangles = new int[rayCount*3];
-
-        vertices[0] = origin;
 
         int vertexIndex = 1;
         int triangleIndex = 0;
@@ -49,10 +52,27 @@ public class FoVPlayerLose : MonoBehaviour
         mesh.triangles = triangles;
 
         mesh.RecalculateBounds();
+
+        Vector2[] v2 = new Vector2[vertices.Length];
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 v3 = vertices[i];
+            v2[i] = new Vector2(v3.x, v3.y);
+        }
+        polyCollider.SetPath(0, v2);
     }
     private Vector3 GetVectorFromAngle(float angle)
     {
         float angleRad = angle * (Mathf.PI / 180f);
         return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == PlayerTag)
+        {
+            Debug.Log("Game Has Lost, Reset Map");
+            //Game Lose Play animation? Reset
+        }
     }
 }
