b3543765de03f953bec6fbc1701316811e8e2f7c
Nick D <nicolas.k.delbue@gmail.com>
2026-08-08 15:34:24 -0400

    Start of FOV
    Working version of non moving non blocking fov effect

---
+++ HiddenGameJam/Assets/Scripts/Lose/FoVPlayerLose.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+public class FoVPlayerLose : MonoBehaviour
+{
+    private void Start()
+    {
+        Mesh mesh = new Mesh();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 2;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDist = 5f;
+
+
+        Vector3[] vertices = new Vector3[rayCount+1+1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount*3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 angleVertex = GetVectorFromAngle(angle);
+            Vector3 vertex = origin + (angleVertex * viewDist);
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateBounds();
+    }
+    private Vector3 GetVectorFromAngle(float angle)
+    {
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+}
